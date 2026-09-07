8abf3e969ed38e54d4c088bb7cc438ac112dabe8
areeb065 <mohammedareeb323@gmail.com>
2026-09-07 06:01:35 +0000

    memory explained!

---
+++ 01_Basics/memory.js
@@ -0,0 +1,7 @@
+//Stack(primitive)=> we get copy, Heap (Non-Primitive)=> we get reference!
+let myName="Areeb"
+let anothername=myName
+ anotherName="MdAreeb"
+
+ console.log(myName)
+ console.log(anotherName)
