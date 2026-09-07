ba31e5fce97f68de9bae04bddf2dec136a3b18b6
areeb065 <mohammedareeb323@gmail.com>
2026-09-07 04:12:59 +0000

    variables and datatypes are done!

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newr version
+//alert(2+3) we are usinig nodejs not browser !
+console.log("Hey !")
+
+/*
+number,strings, booleean,null =>standalone value(khali hai)
+undefine=>not define
+symbol=>unique
+*/
+console.log(typeof(null)) //object !!
+console.log(typeof(undefined))//undefined !!
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144554
+let accountEmail="Areeb@gmail.com"
+var accountPassword="14233"
+/*
+preffer not to use var
+because of issue in block scope and functional scope!
+
+*/
+let accountState;
+accountCity="Banglore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
