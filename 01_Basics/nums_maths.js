3493111481554388ac9c032a507114587c823e44
areeb065 <mohammedareeb323@gmail.com>
2026-09-07 17:37:47 +0000

    math and num

---
+++ 01_Basics/nums_maths.js
@@ -0,0 +1,24 @@
+const score=400
+console.log(score)
+
+const balance= new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(typeof(balance))
+
+const otherNumber=38423.8923
+console.log(otherNumber.toPrecision(4))
+
+const hundreds=10000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++Maths++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4,44))
+console.log(Math.floor(4.9))
+
+console.log((Math.random()*10)+1)
+//math.random always come inbetween 0 to 1 !
