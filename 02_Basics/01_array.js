275711a9ccd18cb43b171a43af27cf623f1a5c18
areeb065 <mohammedareeb323@gmail.com>
2026-09-08 04:34:17 +0000

    some more methods

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -24,3 +24,22 @@ console.log("B",arr)
 
 const myn2=arr.splice(1,3)
 console.log(myn2)
+
+//some more methods
+const marvel_heros=["THor","IronMan","SpiderMan"]
+const dc_heros=["superMan","flash","batsMan"]
+
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros)
+//[ 'THor', 'IronMan', 'SpiderMan', [ 'superMan', 'flash', 'batsMan' ] ]
+
+//const all_heros=marvel_heros.concat(dc_heros) //concat
+//console.log(all_heros)
+
+const all_new_heros=[...marvel_heros, ...dc_heros] //concat and spread operater
+console.log(all_new_heros)
+
+const nested_array=[1,2,3,[4,5],7,[8,9,10,[5,7]]]
+const real_array=nested_array.flat(Infinity)
+console.log(real_array) //flat
+
